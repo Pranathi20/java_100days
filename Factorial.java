452624afba1b68d218f38bd8624b8a323a8c6674
Pranathi20 <69498026+Pranathi20@users.noreply.github.com>
2020-08-17 14:50:02 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+package File1;
+import java.util.scanner;
+public class Factorial
+{
+	public static void main(String[] args) 
+	{
+		  int i,fact=1;  
+		  int n=5;   
+		  for(i=1;i<=n;i++)
+		  {    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+n+" is: "+fact);    
+		 }  
+		}
+		
---
+++ Pos_negative.java
@@ -0,0 +1,25 @@
+package File1;
+import java.util.*;
+public class Pos_negative 
+{
+	public static void main(String[] args) 
+	{
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("enter size:");
+	  int n = sc.nextInt();  // 3
+	  
+	  int a[] = new int[n];
+	  
+	  for(int i = 0; i <n ; i++)  // i is local var
+	  {
+		  a[i] = sc.nextInt();
+	  }
+	  
+	  for(int i=0; i<n ; i++)
+	  {
+		  if(a[i] > 0)
+			  System.out.println(a[i]+" is Positive");
+		  else
+			  System.out.println(a[i]+" is Negative");
+	  }
+	}}
