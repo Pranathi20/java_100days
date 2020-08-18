258aa455a7cb34a5a1e9af3484d7e45baad878e3
Pranathi20 <69498026+Pranathi20@users.noreply.github.com>
2020-08-18 15:36:43 +0530

    Add files via upload

---
+++ Amstrong_num.java
@@ -0,0 +1,23 @@
+package File1;
+import java.util.*;
+public class Amstrong_num {
+
+	public static void main(String[] args) {
+		int c=0,a,temp;
+		int n=153;
+		temp=n;
+		while(n>0)
+		{
+			a=n%10;
+			n=n/10;
+			c=c+(a*a*a);
+			if(temp==c)
+				System.out.println("Amstrong number");
+				else
+				System.out.println("not an amstrong number");
+			}
+		}
+
+	
+
+}
