f1d203120ec222436d45260b4419b6e4d4e7b6cd
Pranathi20 <69498026+Pranathi20@users.noreply.github.com>
2020-09-27 11:46:27 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+package Patterns;
+import java.util.*;
+public class Pattern1
+{
+		public static void main(String[] args) {
+			for(int i =1; i<=5;i++) // rows
+			{
+				for(int j=1; j<=5;j++) // cols
+				{
+					System.out.print(i); // 11111
+				}
+				System.out.println();
+			}
+
+	}
+
+}
---
+++ Pattern2.java
@@ -0,0 +1,15 @@
+package Patterns;
+public class Pattern2 {
+	public static void main(String[] args) {
+		char i;
+		char j;
+		for( i ='A'; i<='E';i++) 
+		{
+			for( j='A'; j<='E';j++) 
+			{
+				System.out.print(j); 
+			}
+			System.out.println();
+		}
+
+}}
