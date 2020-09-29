719dad5b442a3561ec786228d0619db32b4193e4
Pranathi20 <69498026+Pranathi20@users.noreply.github.com>
2020-09-29 14:38:17 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		for(int i =1; i<=5;i++) // 5 rows
+		{
+			for(int j=1; j<=i;j++) // cols
+			{
+				System.out.print("*"); //*
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Pattern6.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		for(int i =1; i<=5;i++) //5 rows
+		{
+			for(int j=1; j<=i;j++) // cols
+			{
+				System.out.print(j); //1
+			}
+			System.out.println();
+
+	}
+
+}}
