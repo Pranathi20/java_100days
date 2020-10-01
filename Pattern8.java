6c819c7495ce508b4efe32e66876521b43290f58
Pranathi20 <69498026+Pranathi20@users.noreply.github.com>
2020-10-01 09:17:26 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Pattern8 {
+
+	public static void main(String[] args) {
+		for(int i =1; i<=5;i++) // 5 rows, i=2
+		{
+			for(int j=5; j>=i;j--) // 5 cols
+			{
+				System.out.print("*"); //*****
+			}
+			System.out.println();
+		}
+
+	}
+
+}
