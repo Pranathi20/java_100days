ba28dd7fe747662339acada3df9a52b5e5661fa6
Pranathi20 <69498026+Pranathi20@users.noreply.github.com>
2020-10-03 15:16:18 +0530

    Add files via upload

---
+++ Pattern_9.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class Pattern_9{
+   public static void main(String[] args) {
+	for(int i = 5; i>=1;i--) // 5 rows, i = 5
+	{
+		for(int j=1; j<=i;j++) // 5 cols
+		{
+			System.out.print(j +" "); //1 2 3 4 5
+		}
+		System.out.println();
+	}
+} }
+
